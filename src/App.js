83095adc4469dd6efda23103e2cad82d12b67633
tanjimTC <tanjimtc74@gmail.com>
2020-04-24 00:57:49 +0600

    Robo friends app progress

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-      super()
+    super();
     this.state = {
       robots: robots,
       SearchField: "",
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) => this.setState({robots:user}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ SearchField: event.target.value });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.SearchField.toLowerCase());
+    });
     return (
-      <div className="tc">
-        <h1>Robo Friends</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots} />
+      <div className="tc w-90 center ">
+        <h1 className="f1">Robo Friends</h1>
+        <SearchBox SearchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({SearchChange}) => {
     return (
         <div>
-            <input className="pa2 tc mb3 ba b--green bg-lightest-blue" type="SearchBox" name="search" placeholder="Search Robots"/>
+            <input className="pa2 tc mb3 ba b--green bg-lightest-blue" type="SearchBox" name="search" placeholder="Search Robots"
+            onChange = {SearchChange}
+            />
         </div>
     );
 };
